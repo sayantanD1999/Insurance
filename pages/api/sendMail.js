d8e31ea7ee7aaa5273a6fc03e49631c8d9b5e60b
Sayan Ghosh <sayanghosh8888@gmail.com>
2024-01-27 21:14:53 +0530

    refactored api

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { mailOptions, transporter } from "@/config/nodemailer";
+import { mailOptions, transporter } from "@/mail-util/nodemailer";
 
 export default async function handler(req, res) {
   if (req.method == "POST") {
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         ...mailOptions,
         subject: "Lead Contact",
         text: "Conatct form",
-        html: `<h1> Name -  ${data.data.name} <br/> Phone - ${data.data.phone}}</h1>`,
+        html: `<h1> Name -  ${data.data.name} <br/> Phone - ${data.data.phone}</h1>`,
       });
       res.status(200).json(data);
       return res
